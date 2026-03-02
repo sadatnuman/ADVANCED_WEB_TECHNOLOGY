2ef9118cc181974e81de7a3dee93e0d30ec97073
MD. Nazmus Sadat Numan <nazmussadatnuman92@gmail.com>
2026-03-03 02:27:58 +0600

    Initial commit

---
+++ Nestjs_Lab_Task_01/course-management-api/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CourseModule } from './course/course.module';
+
+@Module({
+  imports: [CourseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+
